80d133c44244ecb01312b593f4fc0709bee03672
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-04-21 14:29:10 +0100

    Removing bad instances

--- InvidiousRedirect.user.js
+++ InvidiousRedirect.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
-// @name 		Invidious Redirect
-// @namespace 	Backend
-// @description	Redirects Youtube URLs to a random Invidious instance
-// @version		1.0
-// @copyright   2021 Chopper
-// @run-at 		document-start
-// @include 	http://www.youtube.com/*
-// @include		https://www.youtube.com/*
-// @include 	http://m.youtube.com/*
-// @include 	https://m.youtube.com/*
-// @grant 		none
+// @description Redirects Youtube URLs to a random Invidious instance
+// @name Invidious Redirect
+// @namespace Backend
+// @include http://www.youtube.com/*
+// @include https://www.youtube.com/*
+// @include http://m.youtube.com/*
+// @include https://m.youtube.com/*
+// @version 1.1
+// @run-at document-start
+// @grant none
 // ==/UserScript==
 
 //Your list of preferred Invidious instances
-const urls = ["invidious.snopyta.org", "yewtu.be", "invidious.tube", "invidious.kavin.rocks", "invidious.048596.xyz", "vid.puffyan.us ", "ytprivate.com", "inv.skyn3t.in", "invidious.himiko.cloud", "tube.incog.host", "invidious.cmh.pw"];
+const urls = ["invidious.snopyta.org", "yewtu.be", "invidious.tube", "invidious.kavin.rocks", "invidious.048596.xyz", "vid.puffyan.us ", "ytprivate.com", "inv.skyn3t.in", "tube.incog.host", "invidious.cmh.pw"];
 
 //Valid random number
 const random = Math.floor(Math.random() * urls.length);
