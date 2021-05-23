e34c97953859911efe88444770261110c5114f2d
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2021-05-23 13:36:47 +0100

    Update
    Added more instances of invidious.
    Changed default quality to hd720 for consistency of quality. Dash varied too much for me.
    Added more sites for the script to replace URLs in.
    Increased the replacement rate of links to 1 second instead of 5 seconds.

--- InvidiousReplacer.user.js
+++ InvidiousReplacer.user.js
@@ -2,25 +2,26 @@
 // @name 		Invidious Replace
 // @description Replaces Youtube URLs with a random Invidious instance
 // @namespace 	InvidReplace
-// @version 	1.0
+// @version 	1.1
 // @run-at 		document-end
 // @grant 		none
 // @include 	http://www.youtube.com/*
 // @include 	https://www.youtube.com/*
-// @include     *watch?v=*
+// @include   https://www.startpage.com/sp/*
+// @include   https://www.startpage.com/do/dsearch?query=*
+// @include   https://duckduckgo.com/?q=*
+// @include   *watch?v=*
 // @include 	http://m.youtube.com/*
 // @include 	https://m.youtube.com/*
 // ==/UserScript==
 
 //Your list of preferred Invidious instances
-const urls = ["yewtu.be", "invidious.tube", "invidious.kavin.rocks", "invidious.048596.xyz", "vid.puffyan.us ", "ytprivate.com", "inv.skyn3t.in", "tube.incog.host", "invidious.cmh.pw", "invidious.silkky.cloud"];
+const urls = ["yewtu.be", "invidious.tube", "invidious.kavin.rocks", "invidious.048596.xyz", "vid.puffyan.us ", "ytprivate.com", "tube.incog.host", "invidious.cmh.pw", "invidious.silkky.cloud", "invidious.snopyta.org", "invidious.exonip.de", "notyoutube.org", "inv.riverside.rocks"];
 
 //Your preferred Invidious parameters here:
-const myParams = '&speed=1.25&dark_mode=true&thin_mode=true&quality=dash'
+const myParams = '&speed=1.25&dark_mode=true&thin_mode=true&quality=hd720'
 //Visit https://github.com/iv-org/documentation/blob/master/List-of-URL-parameters.md to find the list of parameters available.
 
-//Valid random number
-const random = Math.floor(Math.random() * urls.length);
 
 //Current URL (watch? URL)
 var url = window.location.href;
@@ -32,10 +33,11 @@ javascript:setInterval(function(){
   
   for (var i=0;i<links.snapshotLength;i++)
   {
+    var random = Math.floor(Math.random() * urls.length);
     var thisLink = links.snapshotItem(i);
     thisLink.href = thisLink.href.replace(RegExp('www.youtube.com'),(random, urls[random]));
   }  
-},5000);
+},1000);
 
 //Check if URL contains "watch?" and doesn't contain "youtube"
 if((url.indexOf('watch?') > 0) && (url.indexOf('youtube') < 0))
